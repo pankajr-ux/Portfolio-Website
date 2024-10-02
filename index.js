7b002027b580e315a24eb62141a8be776b5b6ae1
Pankaj <145121755+pankajr-ux@users.noreply.github.com>
2024-10-02 23:22:32 +0530

    Add files via upload
    Prototype of portfolio website, For Learning CSS, and Owl Carousel

---
+++ index.js
@@ -0,0 +1,14 @@
+// window scroll
+// $(window).on("scroll", function() {
+//     var scrollTop = $(window).scrollTop();
+//     if(scrollTop >= 80) {
+//         $('body').addClass('fixed-header');
+//     } else {
+//         $('body').rmoveClass('fixed-header')
+//     }
+// });
+
+
+
+
+
